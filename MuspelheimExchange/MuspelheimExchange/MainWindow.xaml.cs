64f93a7d4d0a6bdbd7faa0221baa15ef235f9185
Nastro1995 <aobredell1@gmail.com>
2018-11-03 11:47:24 +0200

    UI Changes.

--- MuspelheimExchange/MuspelheimExchange/MainWindow.xaml.cs
+++ MuspelheimExchange/MuspelheimExchange/MainWindow.xaml.cs
@@ -71,5 +71,21 @@ namespace MuspelheimExchange
                  */
             }
         }
+
+        private void Bar_ShowHide_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (Bar_ShowHide_Btn.IsChecked == true)
+            {
+                Bar_ShowHide_Btn.Content = @"\/";
+                Bar_Border.Visibility = Visibility.Collapsed;
+                Bar_ShowHide_Btn.ToolTip = "Show Bar";
+            }
+            else
+            {
+                Bar_ShowHide_Btn.Content = @"/\";
+                Bar_Border.Visibility = Visibility.Visible;
+                Bar_ShowHide_Btn.ToolTip = "Hide Bar";
+            }
+        }
     }
 }
